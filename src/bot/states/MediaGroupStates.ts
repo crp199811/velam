9e7e9fac3c81c97b777efa45003eaca3589ba5fb
lirym <algeb1998@gmail.com>
2026-01-05 13:27:36 +0200

    Delete src directory

--- src/bot/states/MediaGroupStates.ts
+++
@@ -1,9 +0,0 @@
-const mediaGroupStates = new Map<string, {
-    timeoutId?: NodeJS.Timeout,
-    medias: string[],
-    media_types: Array<"photo" | "video">, 
-    caption: string,
-    message_id: number
-}>();
-
-export default mediaGroupStates;
--- src/bot/states/redis.ts
+++
@@ -1,5 +0,0 @@
-// import { createClient } from "redis";
-
-// export default await createClient()
-//     .on("error", err => console.log("Error | Creating redis client got error\n", err))
-//     .connect();
